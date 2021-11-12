9463e8518d89d36c83581370ce060241ddda96b4
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-12 09:31:28 -0600

    Added create ToDo

--- server/routes/todo.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-ToDo = APIRouter()
-
-
-@ToDo.post("/todo")
-def create_ToDo():
-    return "create_ToDo"
---
+++ server/routes/todoRouter.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+from models.todoModel import ToDo
+from config.database import collection
+from schemas.todoSchema import todosEntity,todoEntity
+from bson.objectid import ObjectId
+
+todoRouter = APIRouter()
+
+
+@todoRouter.post("/todo")
+def create_ToDo(todo: ToDo):
+    new_ToDo = dict(todo)
+    del new_ToDo["id"]
+    collection.insert_one(new_ToDo).inserted_id
+    return "ToDo created"
