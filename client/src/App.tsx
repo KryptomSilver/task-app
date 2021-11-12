46e062575dc33e3dedf38ed5ed70180e5ffab635
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-12 11:06:28 -0600

    Added routes and component render

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,20 @@
 import React from "react";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PageRender from "./PageRender";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <Router>
+        <Routes>
+          <Route path="/" element={<PageRender />} />
+          <Route path="/:page" element={<PageRender />} />
+          <Route path="/:page/:slug" element={<PageRender />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
