91572394805a674606bf89d71557658c223590c9
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-12 16:33:07 -0600

    Added page create todo

---
+++ client/src/pages/create_todo.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CardFormAdd from "../components/cards/CardFormAdd";
+
+const CreateTODO = () => {
+  return (
+    <div className="d-flex justify-content-center align-items-center">
+      <CardFormAdd />
+    </div>
+  );
+};
+
+export default CreateTODO;
