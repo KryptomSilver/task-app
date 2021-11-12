84fb75a4ec708b8d2aa1ccf141729b4d0f2ab695
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-12 11:06:58 -0600

    Page index and component NotFound

---
+++ client/src/components/global/NotFound.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const NotFound = () => {
+  return (
+    <div className="vh-100 d-flex justify-content-center align-items-center">
+      <h2 className="position-absolute text-secondary">404 | NotFound</h2>
+    </div>
+  );
+};
