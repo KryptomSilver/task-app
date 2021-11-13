c8936b90a1c2798ee2f921e38c7019c6b11189ca
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-12 21:51:07 -0600

    Added TODO reducer and interface

--- client/src/interfaces/interfaces.ts
+++ client/src/interfaces/interfaces.ts
@@ -2,3 +2,11 @@ export interface IParams {
   page?: string;
   slug?: string;
 }
+export interface IToDo {
+  _id: string;
+  name: string;
+  description: string;
+  state: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
--- client/src/redux/reducers/todoReducer.ts
+++ client/src/redux/reducers/todoReducer.ts
@@ -1,7 +1,28 @@
-const todoReducer = (state = {}, action) => {
+import { IToDo } from "../../interfaces/interfaces";
+import {
+  CREATE_TODO,
+  DELETE_TODO,
+  GET_TODOS,
+  IToDoType,
+  UPDATE_TODO,
+} from "../types/todoTypes";
+
+const todoReducer = (state: IToDo[] = [], action: IToDoType): IToDo[] => {
   switch (action.type) {
+    case CREATE_TODO:
+      return [action.payload, ...state];
+    case UPDATE_TODO:
+      return state.map((todo: IToDo) =>
+        todo._id === action.payload._id
+          ? { ...todo, name: action.payload.name }
+          : todo
+      );
+    case GET_TODOS:
+      return action.payload;
+    case DELETE_TODO:
+      return state.filter((TODO: IToDo) => TODO._id !== action.payload);
     default:
-      break;
+      return state;
   }
 };
 export default todoReducer;
